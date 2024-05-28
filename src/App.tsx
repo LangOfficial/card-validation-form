c29b5f8973a872350d161764b75602c0eac34b7b
Langli <Langliland64@gmail.com>
2024-05-28 13:28:04 -0700

    forced to put svg

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import Background from "./components/Background";
 import InputSection from "./components/InputSection";
 import Error from "./components/Error";
 
-import Checkmark from "./images/icon-complete.svg";
-
 export interface CardContextProps {
   cardName: string;
   cardNumber: string;
@@ -120,7 +118,6 @@ function App() {
       validCount++;
     }
 
-
     if (Number(expiryYear) < 24) {
       setExpiryYearError("Please enter a valid year");
       setisExpiryYearError(true);
@@ -128,7 +125,7 @@ function App() {
       setisExpiryYearError(false);
       validCount++;
     }
-    
+
     if (!expiryMonth) {
       setExpiryYearError("Please enter a valid year");
       setisExpiryMonthError(true);
@@ -169,106 +166,132 @@ function App() {
       >
         <div className="halfxl:flex halfxl:items-center">
           <Background />
-          { !isSuccess ?
-            (<form
-            className="mt-14 flex flex-col gap-5 p-6 halfxl:ml-96 halfxl:w-[30%]"
-            onSubmit={handleSubmit}
-          >
-            <InputSection>
-              <label htmlFor="name-input" className="input-title">
-                Cardholder name
-              </label>
-              <input
-                type="text"
-                value={cardName}
-                onChange={handleCardNameChange}
-                placeholder="e.g. Jane Appleseed"
-                id="name-input"
-                className={`input-field capitalize ${cardNameError ? "!border-red-400" : ""}`}
-              />
-              {cardNameError && <Error text={cardNameError} />}
-            </InputSection>
-            <InputSection>
-              <label htmlFor="card-number-input" className="input-title">
-                Card Number
-              </label>
-              <input
-                type="text"
-                onChange={handleCardNumChange}
-                value={cardNumber}
-                maxLength={19}
-                id="card-number-input"
-                placeholder="e.g. 1234 5678 9123 0000"
-                className={`input-field ${cardNumError ? "!border-red-400" : ""}`}
-              />
-              {cardNumError && <Error text={cardNumError} />}
-            </InputSection>
-            <div className="mb-6 grid grid-cols-last gap-x-4">
+          {!isSuccess ? (
+            <form
+              className="mt-14 flex flex-col gap-5 p-6 halfxl:ml-96 halfxl:w-[30%]"
+              onSubmit={handleSubmit}
+            >
               <InputSection>
-                <label htmlFor="exp-date-input" className="input-title">
-                  Exp. Date (mm/yy)
-                </label>
-                <div className="grid grid-cols-2 gap-x-3">
-                  <input
-                    type="text"
-                    maxLength={3}
-                    value={expiryMonth}
-                    placeholder="MM"
-                    onChange={handleCardMonthChange}
-                    id="exp-date-input"
-                    className={`input-field ${isExpiryMonthError ? "!border-red-400" : ""}`}
-                  />
-                  <input
-                    type="text"
-                    maxLength={2}
-                    placeholder="YY"
-                    value={expiryYear}
-                    onChange={handleChangeYearChange}
-                    className={`input-field ${isExpiryYearError ? "!border-red-400" : ""}`}
-                  />
-                </div>
-                {isExpiryMonthError || isExpiryYearError ? (
-                  <Error text={expiryYearError} />
-                ) : (
-                  ""
-                )}
-              </InputSection>
-              <InputSection>
-                <label htmlFor="cvc-input" className="input-title">
-                  cvc
+                <label htmlFor="name-input" className="input-title">
+                  Cardholder name
                 </label>
                 <input
                   type="text"
-                  placeholder="e.g. 123"
-                  value={CVC}
-                  maxLength={3}
-                  onChange={handleCVCChange}
-                  className={`input-field w-full ${CVCError ? "!border-red-400" : ""}`}
+                  value={cardName}
+                  onChange={handleCardNameChange}
+                  placeholder="e.g. Jane Appleseed"
+                  id="name-input"
+                  className={`input-field capitalize ${cardNameError ? "!border-red-400" : ""}`}
                 />
-                {CVCError && <Error text={CVCError} />}
+                {cardNameError && <Error text={cardNameError} />}
               </InputSection>
-            </div>
-            <button className="w-full rounded-md bg-neutralC-800 py-3 text-neutralC-50">
-              Confirm
-            </button>
-          </form>) : (<main className="items-center halfxl:flex halfxl:w-1/2">
-            <div className="flex flex-col items-center justify-center px-6 halfxl:ml-96 w-full">
-              <img
-                className="mb-12 mt-20"
-                src={Checkmark}
-                alt=""
-              />
-              <h1 className="mb-6 text-4xl uppercase tracking-widest">
-                Thank You!
-              </h1>
-              <p className="mb-5 text-neutralC-500">
-                We added your card details
-              </p>
+              <InputSection>
+                <label htmlFor="card-number-input" className="input-title">
+                  Card Number
+                </label>
+                <input
+                  type="text"
+                  onChange={handleCardNumChange}
+                  value={cardNumber}
+                  maxLength={19}
+                  id="card-number-input"
+                  placeholder="e.g. 1234 5678 9123 0000"
+                  className={`input-field ${cardNumError ? "!border-red-400" : ""}`}
+                />
+                {cardNumError && <Error text={cardNumError} />}
+              </InputSection>
+              <div className="mb-6 grid grid-cols-last gap-x-4">
+                <InputSection>
+                  <label htmlFor="exp-date-input" className="input-title">
+                    Exp. Date (mm/yy)
+                  </label>
+                  <div className="grid grid-cols-2 gap-x-3">
+                    <input
+                      type="text"
+                      maxLength={3}
+                      value={expiryMonth}
+                      placeholder="MM"
+                      onChange={handleCardMonthChange}
+                      id="exp-date-input"
+                      className={`input-field ${isExpiryMonthError ? "!border-red-400" : ""}`}
+                    />
+                    <input
+                      type="text"
+                      maxLength={2}
+                      placeholder="YY"
+                      value={expiryYear}
+                      onChange={handleChangeYearChange}
+                      className={`input-field ${isExpiryYearError ? "!border-red-400" : ""}`}
+                    />
+                  </div>
+                  {isExpiryMonthError || isExpiryYearError ? (
+                    <Error text={expiryYearError} />
+                  ) : (
+                    ""
+                  )}
+                </InputSection>
+                <InputSection>
+                  <label htmlFor="cvc-input" className="input-title">
+                    cvc
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="e.g. 123"
+                    value={CVC}
+                    maxLength={3}
+                    onChange={handleCVCChange}
+                    className={`input-field w-full ${CVCError ? "!border-red-400" : ""}`}
+                  />
+                  {CVCError && <Error text={CVCError} />}
+                </InputSection>
+              </div>
               <button className="w-full rounded-md bg-neutralC-800 py-3 text-neutralC-50">
-                Continue
+                Confirm
               </button>
-            </div>
-          </main>)}
+            </form>
+          ) : (
+            <main className="items-center halfxl:flex halfxl:w-1/2">
+              <div className="flex w-full flex-col items-center justify-center px-6 halfxl:ml-96">
+                <svg
+                  className="mb-12 mt-20"
+                  width="80"
+                  height="80"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle cx="40" cy="40" r="40" fill="url(#a)" />
+                  <path
+                    d="M28 39.92 36.08 48l16-16"
+                    stroke="#fff"
+                    stroke-width="3"
+                  />
+                  <defs>
+                    <linearGradient
+                      id="a"
+                      x1="-23.014"
+                      y1="11.507"
+                      x2="0"
+                      y2="91.507"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stop-color="#6348FE" />
+                      <stop offset="1" stop-color="#610595" />
+                    </linearGradient>
+                  </defs>
+                </svg>
+
+                <h1 className="mb-6 text-4xl uppercase tracking-widest">
+                  Thank You!
+                </h1>
+                <p className="mb-5 text-neutralC-500">
+                  We added your card details
+                </p>
+                <button className="w-full rounded-md bg-neutralC-800 py-3 text-neutralC-50">
+                  Continue
+                </button>
+              </div>
+            </main>
+          )}
         </div>
       </CardContext.Provider>
     </>
