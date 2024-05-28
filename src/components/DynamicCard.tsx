b45a714d178dce93004ba9c121c8f47ab29df923
Langli <Langliland64@gmail.com>
2024-05-28 13:15:59 -0700

    fixed

--- src/components/DynamicCard.tsx
+++ src/components/DynamicCard.tsx
@@ -1,12 +1,18 @@
 // import React, { Dispatch, SetStateAction } from "react";
 
 import front from "../images/bg-card-front.png";
-import {useCardContext, CardContextProps} from "../App";
+import { useCardContext } from "../App";
 
 const DynamicCard = () => {
-  const {cardName, cardNumber, expiryMonth, expiryYear }: CardContextProps = useCardContext();
+  const cardContext = useCardContext();
 
-  const cardNumberSplitted = cardNumber.split(' ');
+  if (!cardContext) {
+    throw new Error("CardContext is not provided");
+  }
+
+  const { cardName, cardNumber, expiryMonth, expiryYear } = cardContext;
+
+  const cardNumberSplitted = cardNumber.split(" ");
   return (
     <div
       className="absolute bottom-0 left-4 min-h-48 w-[20rem] translate-y-[25%] rounded-lg p-5 text-neutralC-50 sm:left-auto sm:w-[22rem] halfxl:bottom-[50%] halfxl:right-0 halfxl:translate-x-1/4 halfxl:translate-y-0 halfxl:p-7"
@@ -17,17 +23,17 @@ const DynamicCard = () => {
         <div className="h-5 w-5 rounded-full border-2 border-neutralC-50"></div>
       </div>
       <div className="mb-5 flex gap-x-3 text-xl font-medium">
-        <p>{cardNumberSplitted[0] || '0000'}</p>
-        <p>{cardNumberSplitted[1] || '0000'}</p>
-        <p>{cardNumberSplitted[2] || '0000'}</p>
-        <p>{cardNumberSplitted[3] || '0000'}</p>
+        <p>{cardNumberSplitted[0] || "0000"}</p>
+        <p>{cardNumberSplitted[1] || "0000"}</p>
+        <p>{cardNumberSplitted[2] || "0000"}</p>
+        <p>{cardNumberSplitted[3] || "0000"}</p>
       </div>
       <div className="flex">
         <p className="mr-auto text-sm">{cardName || "Jane Appleseed"}</p>
         <p className="text-sm">
-          <span>{expiryMonth || '00'}</span>
+          <span>{expiryMonth || "00"}</span>
           <span>/</span>
-          <span>{expiryYear || '00'}</span>
+          <span>{expiryYear || "00"}</span>
         </p>
       </div>
     </div>
